2819e437b72593ebad6dbbca3d4bdd61321cd16d
yihune21 <zewdieyihune@gmail.com>
2024-08-02 01:13:10 +0300

    building turn logic

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const board__items = document.querySelectorAll(".game__board_item");
+let turn = "Player 1";
+let isGameOver = false;
+
+board__items.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (turn === "Player 1") {
+      item.innerHTML = "X";
+      turn = "Player 2";
+    } else {
+      item.innerHTML = "O";
+      turn = "Player 1";
+    }
+  });
+});
