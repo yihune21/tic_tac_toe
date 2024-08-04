029967c2a08b2ed2b0c604dc6e326c6c10877c97
yihune21 <zewdieyihune@gmail.com>
2024-08-04 17:36:16 +0300

    implement turn change and disabled multiple click

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let isGameOver = false;
 
 board__items.forEach((item) => {
   item.addEventListener("click", () => {
+    if (item.textContent !== "") return;
     if (turn === "Player 1") {
       item.innerHTML = "X";
       turn = "Player 2";
